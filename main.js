1d4c8212fadf2e80f3970e5dba7b95f720945499
zglrx <79danil@gmail.com>
2017-09-20 00:27:39 +0300

    Wheather is showed)

--- main.js
+++ main.js
@@ -1,47 +1,53 @@
+var URL = '';
+
+navigator.geolocation.getCurrentPosition(CreateURLRequest);
+
 function CreateURLRequest(location) {
     var APPID = 'APPID=b6f815a1cdc5541fe552465bdecb3bde',
         API = 'http://api.openweathermap.org/data/2.5/weather?',
         LAN = '&lat=',
         LON = '&lon=',
-        location = [];
-
-
-
-
-    return API + APPID + LAN + location[0] + LON + location[1];
-};
-
-function getCurrentLocation() {
-    var location = [], options = {
-        enableHighAccuracy: true,
-        timeout: 5000,
-        maximumAge: 0
-    };
-
-    function success(pos) {
-        location = [pos.coords.latitude, pos.coords.longitude];
-    };
-
-    function error(err) {
-        console.warn(`ERROR(${err.code}): ${err.message}`);
-    };
-
-    navigator.geolocation.getCurrentPosition(success, error, options);
-
-    
-
-    return location;
+        position = [];
+    position = [location.coords.latitude, location.coords.longitude];
+    URL = API + APPID + LAN + position[0] + LON + position[1];
 }
 
-function sendWeatherRequest() {
+
+$(document).ready(function () {
+    $('button').click(function () {
+        if (URL == '') {
+            $('.main').html('<h3>We couldn\'t show anything cause you didn\'t allow get your geolocation</h3>');
+        } else {
+            $('button').css('display', 'none');
+            sendWeatherRequest(URL);
+        }
+    });
+});
+
+function sendWeatherRequest(URL) {
     var XHR = ("onload" in new XMLHttpRequest()) ? XMLHttpRequest : XDomainRequest;
     var xhr = new XHR();
-    xhr.open('GET', CreateURLRequest(), true);
+    xhr.open('GET', URL, true);
     xhr.onload = function () {
-        alert(this.responseText);
+        showWeatherInfo(this.responseText);
     }
     xhr.onerror = function () {
-        alert('Ошибка ' + this.status);
+        $('#city').html('Ошибка ' + this.status);
+        $('.lead').remove();
     }
     xhr.send();
-};
+}
+
+
+
+function showWeatherInfo(weatherData) {
+    weatherData = jQuery.parseJSON(weatherData);
+    var city = weatherData.name || 'Data doesn\'t catched',
+        temperature = weatherData.main.temp || 'Data doesn\'t catched',
+        icon = '<img src="http://openweathermap.org/img/w/' + weatherData.weather[0].icon + '.png" />';
+    $(document).ready(function () {
+        $('#city').html('Location: ' + city);
+        $('.lead').html(icon);
+        $('.temperature').html('Celsium: ' + Math.round(temperature - 273.15));
+    });
+}
